0287ed861e8a042f43f1b0ff58a525e2ba2bf88f
Sven Buschke <sven@buschke.com>
2023-07-23 19:01:01 +0200

    cleansed code

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'osternrechner.dart';  // Aktualisierter Import
-import 'tankverwalter.dart';  // Aktualisierter Import
+import 'osternrechner.dart';
+import 'tankverwalter.dart';
 
 void main() {
   runApp(const MainApp());
@@ -12,34 +12,36 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Builder(builder: (context) =>
-        Scaffold(
+      home: Builder(
+        builder: (context) => Scaffold(
           appBar: AppBar(
-            title: const Text('Home Page'),
+            title: const Text('Hauptseite'),
           ),
           body: Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                SizedBox(height: 50),  // Abstand hinzufügen
+                SizedBox(height: 50),
                 ElevatedButton.icon(
-                  icon: const Icon(Icons.date_range, size: 30),  // Symbol für Osternrechner
-                  label: const Text('Go to Oster Page'),
+                  icon: const Icon(Icons.date_range, size: 30),
+                  label: const Text('Osternrechner'),
                   onPressed: () {
                     Navigator.push(
                       context,
-                      MaterialPageRoute(builder: (context) => const OsterPage()),
+                      MaterialPageRoute(
+                          builder: (context) => const OsterPage()),
                     );
                   },
                 ),
-                SizedBox(height: 50),  // Abstand hinzufügen
+                SizedBox(height: 50),
                 ElevatedButton.icon(
-                  icon: const Icon(Icons.local_gas_station, size: 30),  // Symbol für Tankverwalter
-                  label: const Text('Go to Tankverwalter'),
+                  icon: const Icon(Icons.local_gas_station, size: 30),
+                  label: const Text('Tankverwalter'),
                   onPressed: () {
                     Navigator.push(
                       context,
-                      MaterialPageRoute(builder: (context) => const Tankverwalter()),  // Aktualisierter Seitenname
+                      MaterialPageRoute(
+                          builder: (context) => const Tankverwalter()),
                     );
                   },
                 ),
